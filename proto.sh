d7b7c44c2bc2df8d1c439030b7fa4938c22a59ea
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-09-19 10:09:46 +0900

    Should auto-generated code be included in the repo??

--- proto.sh
+++ proto.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
-protoc --grpc-web_out=import_style=typescript,mode=grpcwebtext:frontend/grpc -I grpc grpc/topics.proto
+protoc \
+  --grpc-web_out=import_style=typescript,mode=grpcwebtext:frontend/grpc \
+  --go_out=plugins=grpc:backend/grpc_front \
+  -I grpc grpc/topics.proto
