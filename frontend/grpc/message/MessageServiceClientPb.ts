9e6ddba26dd7d6bee6c68b707957336fe71c3eeb
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-09-25 17:52:32 +0900

    Include auto-generated code (this is a bad pattern, but seems to be good for CI IMO)

---
+++ frontend/grpc/message/MessageServiceClientPb.ts
@@ -0,0 +1,54 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+import * as grpcWeb from 'grpc-web';
+
+import {
+  Message,
+  MessageRequest} from './message_pb';
+
+export class MessageServiceClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
+
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'text';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfoSubscribe = new grpcWeb.AbstractClientBase.MethodInfo(
+    Message,
+    (request: MessageRequest) => {
+      return request.serializeBinary();
+    },
+    Message.deserializeBinary
+  );
+
+  subscribe(
+    request: MessageRequest,
+    metadata?: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/MessageService/Subscribe',
+      request,
+      metadata || {},
+      this.methodInfoSubscribe);
+  }
+
+}
---
+++ frontend/grpc/topic/TopicServiceClientPb.ts
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for 
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+import * as grpcWeb from 'grpc-web';
+
+import {
+  TopicInfo,
+  TopicRequest} from './topic_pb';
+
+export class TopicServiceClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
+
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'text';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfoTopic = new grpcWeb.AbstractClientBase.MethodInfo(
+    TopicInfo,
+    (request: TopicRequest) => {
+      return request.serializeBinary();
+    },
+    TopicInfo.deserializeBinary
+  );
+
+  topic(
+    request: TopicRequest,
+    metadata?: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/TopicService/Topic',
+      request,
+      metadata || {},
+      this.methodInfoTopic);
+  }
+
+}
+
