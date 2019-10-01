977284c17be518003bb95c4fb6460069466e7a6e
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-10-01 13:00:20 +0900

    Added model for the db

---
+++ backend/message/model.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Model indicates the model of the message.
+type Model struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	TopicId    primitive.ObjectID
+	SenderName string
+	PostTime   time.Time
+	Profile    string
+	Message    string
+	Host       string
+}
