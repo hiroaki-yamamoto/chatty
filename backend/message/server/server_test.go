e8ed1473ad39535a1be3676bc4b31a168a623a63
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-10-07 14:35:18 +0900

    ginkgo bootstrap (with my sleepy brain...)

---
+++ backend/message/server/server_test.go
@@ -0,0 +1,12 @@
+package server_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/hiroaki-yamamoto/real/backend/message/server"
+)
+
+var _ = Describe("Server", func() {
+
+})
