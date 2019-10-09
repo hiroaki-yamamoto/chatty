acae761cbaf6e472f9402ec5266b3734dc184fd2
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-10-09 09:14:23 +0900

    Added AfterSuite and BeoreSuite hook

--- backend/message/server/server_suite_test.go
+++ backend/message/server/server_suite_test.go
@@ -1,11 +1,15 @@
 package server_test
 
 import (
+	"log"
 	"testing"
 
+	"github.com/hiroaki-yamamoto/real/backend/config"
 	"github.com/hiroaki-yamamoto/real/backend/svrutils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc"
 )
 
 func TestServer(t *testing.T) {
@@ -13,6 +17,26 @@ func TestServer(t *testing.T) {
 	RunSpecs(t, "Server Suite")
 }
 
+var cli *mongo.Client
+var cfg *config.Config
+var svr *grpc.Server
+
 var _ = BeforeSuite(func() {
-	cfg := svrutils.LoadCfg()
+	cfg = svrutils.LoadCfg()
+	cfg.Db.URI = "mongo://real:real@testdb"
+	cfg.Server.Type = "unix"
+	cfg.Server.Addr = "/tmp/real-test.sock"
+	cli = svrutils.ConnectDB(cfg)
+	svr, lis := svrutils.Construct(cfg)
+
+	go func() {
+		if err := svr.Serve(lis); err != nil {
+			log.Panicln("Server Start Failed: ", err)
+		}
+	}()
+})
+
+var _ = AfterSuite(func() {
+	svr.GracefulStop()
+	svrutils.DisconnectDB(cli, &cfg.Db)
 })
