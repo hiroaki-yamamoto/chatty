7f7a7151d18a92014ddc455f358f470968b4b294
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-10-09 12:49:28 +0900

    Added client connection

--- backend/message/server/server_suite_test.go
+++ backend/message/server/server_suite_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/hiroaki-yamamoto/real/backend/config"
+	"github.com/hiroaki-yamamoto/real/backend/rpc"
 	"github.com/hiroaki-yamamoto/real/backend/svrutils"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -17,7 +18,9 @@ func TestServer(t *testing.T) {
 	RunSpecs(t, "Server Suite")
 }
 
-var cli *mongo.Client
+var db *mongo.Client
+var cli rpc.MessageServiceClient
+var clicon *grpc.ClientConn
 var cfg *config.Config
 var svr *grpc.Server
 
@@ -26,7 +29,7 @@ var _ = BeforeSuite(func() {
 	cfg.Db.URI = "mongo://real:real@testdb"
 	cfg.Server.Type = "unix"
 	cfg.Server.Addr = "/tmp/real-test.sock"
-	cli = svrutils.ConnectDB(cfg)
+	db = svrutils.ConnectDB(cfg)
 	svr, lis := svrutils.Construct(cfg)
 
 	go func() {
@@ -34,9 +37,16 @@ var _ = BeforeSuite(func() {
 			log.Panicln("Server Start Failed: ", err)
 		}
 	}()
+	if con, err := grpc.Dial(cfg.Server.Addr); err != nil {
+		log.Panicln("Connection Dial Failed: ", err)
+	} else {
+		cli = rpc.NewMessageServiceClient(con)
+		clicon = con
+	}
 })
 
 var _ = AfterSuite(func() {
+	clicon.Close()
 	svr.GracefulStop()
-	svrutils.DisconnectDB(cli, &cfg.Db)
+	svrutils.DisconnectDB(db, &cfg.Db)
 })
