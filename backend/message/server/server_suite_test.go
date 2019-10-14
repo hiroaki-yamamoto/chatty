c1a456a90aa730ec87bda14d26c26bc8ec0117eb
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-10-14 15:27:18 +0900

    Reset db URI

--- backend/message/server/server_suite_test.go
+++ backend/message/server/server_suite_test.go
@@ -30,7 +30,7 @@ var svr *grpc.Server
 
 var _ = BeforeSuite(func() {
 	cfg = svrutils.LoadCfg()
-	cfg.Db.URI = "mongodb://real:real@testdb0,testdb1,testdb2/?replicaSet=db"
+	cfg.Db.URI = "mongodb://real:real@testdb"
 	cfg.Server.Type = "unix"
 	cfg.Server.Addr = "/tmp/" + PKGNAME + ".sock"
 	db = svrutils.ConnectDB(cfg).Database(cfg.Db.Name)
