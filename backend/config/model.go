0f9e832e4ec62c0ddbcc83aeb6e14a423d284d85
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-10-29 16:13:49 +0900

    Removed CI config

--- backend/config/model.go
+++ backend/config/model.go
@@ -1,21 +1,8 @@
 package config
 
-import (
-	"context"
-	"time"
-)
-
 // Config represents a configuration.
 type Config struct {
-	Timeout time.Duration
-	Db      DB
-	Server  Server
-	Broker  Broker
-}
-
-// TimeoutContext returns a context set a timeout with DB.Timeout.
-func (me Config) TimeoutContext(ctx context.Context) (
-	context.Context, context.CancelFunc,
-) {
-	return context.WithTimeout(ctx, me.Timeout)
+	Db     DB
+	Server Server
+	Broker Broker
 }
