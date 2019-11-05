24006905a83c4837cbf3f457c8e826270eec5429
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-11-05 10:41:25 +0900

    Introduce ninja to generate from proto file

--- proto.sh
+++ proto.sh
@@ -1,47 +1,27 @@
 #!/bin/sh -e
 # -*- coding: utf-8 -*-
 
-backendDir=backend/rpc
-frontendDir=frontend/rpc
-
-generateBackend() {
-  mkdir -p ${backendDir}
-  # Intermediate protocol between backend and frontend.
-  for fle in grpc/*.proto; do
-    protoc --go_out=plugins=grpc:${backendDir} -I grpc ${fle}
-  done
-  # **Internal** protocol between backend and **backend**
-  for fle in backend/*/grpc/*.proto; do
-    mkdir -p $(dirname $(dirname ${fle}))/rpc/
-    protoc \
-      --go_out=plugins=grpc:$(dirname $(dirname ${fle}))/rpc/ \
-      -I $(dirname ${fle}) ${fle}
-  done
+backend() {
+  ninja -C backend ${@}
 }
 
-generateFrontend() {
-  mkdir -p ${frontendDir}
-  for fle in grpc/*.proto; do
-    out=${frontendDir}/$(basename ${fle/.proto/})
-    mkdir -p ${out}
-    protoc \
-      --grpc-web_out=import_style=typescript,mode=grpcwebtext:${out} \
-      -I grpc ${fle}
-  done
+frontend() {
+  ninja -C frontend ${@}
 }
 
 case $1 in
   "backend")
-    generateBackend
+    backend
     ;;
   "frontend")
-    generateFrontend
+    frontend
     ;;
   "clean")
-    rm -r ${backendDir} ${frontendDir} backend/*/rpc
+    backend -t clean
+    frontend -t clean
     ;;
   *)
-    generateBackend
-    generateFrontend
+    backend
+    frontend
     ;;
 esac
