0a9af3d4950d246a91daa649d3aa0a3d1029eb90
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-11-10 14:01:23 +0900

    Added db count for stats command @ internal server
    btw, I need to move into Gotanda(Tokyo) for my new job but there seems
    to be so few estates that satisfy my demands...
    
    I should have been born at Tokyo...lol

--- backend/message/server/internal_server.go
+++ backend/message/server/internal_server.go
@@ -1,18 +1,53 @@
 package server
 
 import (
-	"errors"
-
 	prvRPC "github.com/hiroaki-yamamoto/real/backend/message/rpc"
+	"go.mongodb.org/mongo-driver/bson"
+	pr "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // InternalServer is a server to provide internal information like stats info.
 type InternalServer struct {
+	DB *mongo.Database
+}
+
+func (me *InternalServer) collection() *mongo.Collection {
+	return me.DB.Collection(srvName)
 }
 
 // Stats generates statistics report of the specified message
 func (me *InternalServer) Stats(
 	srv prvRPC.MessageStats_StatsServer,
-) error {
-	return errors.New("Not Implemented Yet")
+) (err error) {
+	var req *prvRPC.StatsRequest
+	var topicID pr.ObjectID
+	var numDoc int64
+	req, err = srv.Recv()
+	if err != nil {
+		return nil
+	}
+	topicID, err = pr.ObjectIDFromHex(req.GetTopicId())
+	if err != nil {
+		return nil
+	}
+	col := me.collection()
+	numDoc, err = col.CountDocuments(srv.Context(), bson.M{"topicid": topicID})
+	if err != nil {
+		return
+	}
+	resp := &prvRPC.StatsResponse{
+		TopicId: topicID.Hex(),
+		NumMsgs: numDoc,
+	}
+	err = srv.Send(resp)
+	if err != nil {
+		return
+	}
+	for {
+		select {
+		case <-srv.Context().Done():
+			return
+		}
+	}
 }
