c93694716df8077d34748fbabc26075af61dc11c
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-11-16 09:21:55 +0900

    Integrated internal server into main.go
    (And I seem to need to give windows migration up...??)

--- backend/message/main.go
+++ backend/message/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	statsRPC "github.com/hiroaki-yamamoto/real/backend/message/rpc"
 	"github.com/hiroaki-yamamoto/real/backend/message/server"
 	"github.com/hiroaki-yamamoto/real/backend/rpc"
 	"github.com/hiroaki-yamamoto/real/backend/svrutils"
+	"sync"
 )
 
 func main() {
+	var svrLock sync.WaitGroup
+	svrLock.Add(2) // main.go will launch 2 servers; public RPC and stats.
 	cfg := svrutils.LoadCfg()
 	pubSvrCfg := cfg.Servers["message"]
+	statsSvrCfg := cfg.Servers["message/stats"]
 	broker := svrutils.InitBroker(cfg)
 	dbcli := svrutils.ConnectDB(cfg)
+	defer svrutils.DisconnectDB(dbcli, &cfg.Db)
 	svr, lis := svrutils.Construct(pubSvrCfg)
+	statsSvr, statsLis := svrutils.Construct(statsSvrCfg)
 	rpc.RegisterMessageServiceServer(
 		svr,
 		&server.Server{
@@ -20,6 +27,17 @@ func main() {
 			Broker:   broker,
 		},
 	)
-	svrutils.Serve(lis, svr, pubSvrCfg)
-	defer svrutils.DisconnectDB(dbcli, &cfg.Db)
+	statsRPC.RegisterMessageStatsServer(statsSvr, &server.InternalServer{
+		DB:     dbcli.Database(cfg.Db.Name),
+		Broker: broker,
+	})
+	go func() {
+		svrutils.Serve(lis, svr, pubSvrCfg)
+		svrLock.Done()
+	}()
+	go func() {
+		svrutils.Serve(statsLis, statsSvr, statsSvrCfg)
+		svrLock.Done()
+	}()
+	svrLock.Wait()
 }
